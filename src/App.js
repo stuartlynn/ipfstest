cbbb4cd9e89ce3f53a79563449e95f29e04adf08
Stuart Lynn <stuart.lynn@gmail.com>
2018-10-07 12:29:54 -0400

    inital commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import IPFS from 'ipfs';
+
+
 class App extends Component {
+  componentWillMount() {
+    console.log('triyng to run')
+    this.ipfs = new IPFS({
+      repo:'datasurvey_test',
+      start: true,
+      EXPERIMENTAL:{
+        pubsub: true
+      },
+      config: {
+        Addresses: {
+          Swarm: ['/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star']
+        }
+      }
+    })
+    this.ipfs.on('ready', ()=>{
+      console.log('ipfs is ready')
+    })
+    this.ipfs.on('error', (e)=>{
+      console.log('error ', e)
+    })
+  }
   render() {
     return (
       <div className="App">
